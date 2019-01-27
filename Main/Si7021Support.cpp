706a7e0c30a4f8e410119da30dcbc22ac1777231
Порохня Дмитрий <spywarrior@gmail.com>
2019-01-27 16:15:48 +0300

    Add files via upload

--- Main/Si7021Support.cpp
+++ Main/Si7021Support.cpp
@@ -22,9 +22,10 @@ HumidityAnswer Si7021::read()
   humidity = sensor.readHumidity();
   temperature = sensor.readTemperature();
   
+  
+/*
   byte humError = (byte) humidity;
   byte tempError = (byte) temperature;
-/*
   if(humError == HTU21D_ERROR || tempError == HTU21D_ERROR)
   {
     dt.IsOK = false;
@@ -33,30 +34,36 @@ HumidityAnswer Si7021::read()
   {
 */  
      dt.IsOK = true;
-     
-    int iTmp = humidity*100;
-    
-    dt.Humidity = iTmp/100;
-    dt.HumidityDecimal = iTmp%100;
 
-    if(dt.Humidity < 0 || dt.Humidity > 100)
-    {
-      dt.Humidity = NO_TEMPERATURE_DATA;
-      dt.HumidityDecimal = 0;
+    if(humidity > -1.0)
+    { 
+      int iTmp = humidity*100;
+      
+      dt.Humidity = iTmp/100;
+      dt.HumidityDecimal = iTmp%100;
+  
+      if(dt.Humidity < 0 || dt.Humidity > 100)
+      {
+        dt.Humidity = NO_TEMPERATURE_DATA;
+        dt.HumidityDecimal = 0;
+      }
     }
-          
-    iTmp = temperature*100;
 
-    int8_t t1 =     iTmp/100;
-    uint8_t t2 = abs(iTmp%100);
+    if(temperature > -200.0)
+    {      
+        iTmp = temperature*100;
     
-    dt.Temperature = t1;
-    dt.TemperatureDecimal = t2;
-
-    if(dt.Temperature < -40 || dt.Temperature > 125)
-    {
-      dt.Temperature = NO_TEMPERATURE_DATA;
-      dt.TemperatureDecimal = 0;
+        int8_t t1 =     iTmp/100;
+        uint8_t t2 = abs(iTmp%100);
+        
+        dt.Temperature = t1;
+        dt.TemperatureDecimal = t2;
+    
+        if(dt.Temperature < -40 || dt.Temperature > 125)
+        {
+          dt.Temperature = NO_TEMPERATURE_DATA;
+          dt.TemperatureDecimal = 0;
+        }
     }
        
 /////////////////////  } 
